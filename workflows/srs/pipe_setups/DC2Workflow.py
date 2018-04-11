13a2c5fd909ee0465dc95f35b28698a4dd53a279
Heather Kelly <heather@slac.stanford.edu>
2018-04-10 17:46:11 -0700

    add makeFpSummary task

--- workflows/srs/pipe_setups/DC2Workflow.py
+++ workflows/srs/pipe_setups/DC2Workflow.py
@@ -30,6 +30,20 @@ def run_processEimage():
             pipeline.createSubstream("processEimageFilter", num, vars)
             num += 1
 
+def run_makeFpSummary():
+    process = pipeline.getProcessInstance("setup_makeFpSummary")
+    vars = HashMap(process.getVariables())
+    workdir = vars.remove("WORK_DIR")
+    filters = vars.remove("FILTERS").split(',')
+    num = 0
+    for filt in filters:
+        nscript = vars.remove('n' + filt + 'scripts')
+        for i in range(1, int(nscript) + 1):
+            script = workdir + "/02-processEimage/scripts/%s/visit_makeFpSummary_%03d_script.sh" % (filt, i)
+            vars.put("MAKEFP_SCRIPT", script)
+            pipeline.createSubstream("makeFpSummaryFilter", num, vars)
+            num += 1
+
 
 def run_singleFrameDriver():
     process = pipeline.getProcessInstance("setup_singleFrameDriver")
