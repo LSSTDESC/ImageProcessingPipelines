9fca696152f0c953bd5bd80f9eb3c228e62b736a
Nicoals Chotard <nchotard@in2p3.fr>
2018-04-18 15:00:36 +0200

    Typos in setup_datadir.sh

--- workflows/srs/pipe_setups/setup_datadir.sh
+++ workflows/srs/pipe_setups/setup_datadir.sh
@@ -1,9 +1,9 @@
 # EDIT ME
 export RUN="Run1.2p"
 export DMVER="w_2018_15"
-cp /pbs/throng/lsst/software/desc/DC2/$RUN/ImageProcessingPipelines/workflows/srs/pipe_setup/checkpoints.sh .
+cp /pbs/throng/lsst/software/desc/DC2/$RUN/ImageProcessingPipelines/workflows/srs/pipe_setups/checkpoints.sh .
 cp /pbs/throng/lsst/software/desc/DC2/$RUN/ImageProcessingPipelines/workflows/srs/pipe_streams/stream_config.sh .
 source /sps/lsst/software/lsst_distrib/${DMVER}/loadLSST.bash
-export PATH=$PATH:/pbs/throng/lsst/software/desc/DC2/$RUN/ImageProcessingPipelines/workflows/srs/pipe_script/
+export PATH=$PATH:/pbs/throng/lsst/software/desc/DC2/$RUN/ImageProcessingPipelines/workflows/srs/pipe_scripts/
 export VISITDIR=/sps/lsst/datasets/desc/DC2/$RUN/
 createIngestFileList.py $VISITDIR --recursive
