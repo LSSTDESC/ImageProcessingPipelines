7bc34918d2981a427d8cc287944c238228edf768
Nicoals Chotard <nchotard@in2p3.fr>
2018-04-18 14:44:10 +0200

    New setup file for output data directory

---
+++ workflows/srs/pipe_setups/setup_datadir.sh
@@ -0,0 +1,8 @@
+# EDIT ME
+export RUN="Run1.2p"
+export DMVER="w_2018_15"
+cp /pbs/throng/lsst/software/desc/DC2/$RUN/ImageProcessingPipelines/workflows/srs/pipe_setup/checkpoints.sh .
+source /sps/lsst/software/lsst_distrib/${DMVER}/loadLSST.bash
+export PATH=$PATH:/pbs/throng/lsst/software/desc/DC2/$RUN/ImageProcessingPipelines/workflows/srs/pipe_script/
+export VISITDIR=/sps/lsst/datasets/desc/DC2/$RUN/
+createIngestFileList.py $VISITDIR --recursive
