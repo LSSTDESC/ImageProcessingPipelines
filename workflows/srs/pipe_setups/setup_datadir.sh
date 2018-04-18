7562162594145b7c1e3194b8ae722e94ec802760
Nicoals Chotard <nchotard@in2p3.fr>
2018-04-18 14:52:23 +0200

    Add stream file to setup

--- workflows/srs/pipe_setups/setup_datadir.sh
+++ workflows/srs/pipe_setups/setup_datadir.sh
@@ -2,6 +2,7 @@
 export RUN="Run1.2p"
 export DMVER="w_2018_15"
 cp /pbs/throng/lsst/software/desc/DC2/$RUN/ImageProcessingPipelines/workflows/srs/pipe_setup/checkpoints.sh .
+cp /pbs/throng/lsst/software/desc/DC2/$RUN/ImageProcessingPipelines/workflows/srs/pipe_streams/stream_config.sh .
 source /sps/lsst/software/lsst_distrib/${DMVER}/loadLSST.bash
 export PATH=$PATH:/pbs/throng/lsst/software/desc/DC2/$RUN/ImageProcessingPipelines/workflows/srs/pipe_script/
 export VISITDIR=/sps/lsst/datasets/desc/DC2/$RUN/
