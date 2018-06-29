2080e3bb211ec2eb4f728c6cf01a55a8844c06a2
Johann Cohen-Tanugi <johann.cohen-tanugi@umontpellier.fr>
2018-06-29 19:02:53 +0200

    fix the switch off of photometry and astrometry

--- workflows/srs/dm_configs/singleFrameDriverConfig_basicnocalib.py
+++ workflows/srs/dm_configs/singleFrameDriverConfig_basicnocalib.py
@@ -2,8 +2,8 @@
 config.processCcd.isr.doDark=False
 config.processCcd.isr.doBias=False
 config.processCcd.isr.doFlat=False
-config.processCcd.calibrate.doPhotoCal=False
-config.processCcd.calibrate.doAstrometry=False
+config.processCcd.calibrate.doPhotoCal=True
+config.processCcd.calibrate.doAstrometry=True
 config.processCcd.isr.doLinearize = False
 config.processCcd.isr.doDefect=False
 
