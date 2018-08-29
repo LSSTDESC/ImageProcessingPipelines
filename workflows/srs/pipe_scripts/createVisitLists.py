a0984435431b92fb8a3d317c6a1f833090444256
Johann Cohen-Tanugi <johann.cohentanugi@gmail.com>
2018-08-29 13:25:13 +0200

    Revert "U/johannct/dev"

--- workflows/srs/pipe_scripts/createVisitLists.py
+++ workflows/srs/pipe_scripts/createVisitLists.py
@@ -62,11 +62,10 @@ if __name__ == "__main__":
         raise IOError("Option idopt must be 'selectid' or 'id'")    
 
     # Load the butler for this input directory
-    # if os.path.exists(os.path.join(args.input,'rerun')):
-    #     datadir=os.path.join(args.input,'rerun',os.environ['RERUN'])
-    # else:
-    #     datadir=args.input
-    datadir = args.input
+    if os.path.exists(os.path.join(args.input,'rerun')):
+        datadir=os.path.join(args.input,'rerun',os.environ['RERUN'])
+    else:
+        datadir=args.input
     print("butler call on dir %s"%datadir)
     butler = dafPersist.Butler(datadir)
     
@@ -120,18 +119,13 @@ if __name__ == "__main__":
         file_to_save = open(all_file, 'w')
         for dataid in fdataids[filt]:
             try:
-                sensor=dataid['detector']
-                file_to_save.write("--%s visit=%i detector=%s\n" % \
-                                (args.idopt, dataid['visit'], sensor))
+                sensor_name='sensor'
+                sensor=dataid[sensor_name]
             except:
-                try:
-                    sensor_name='sensor'
-                    sensor=dataid[sensor_name]
-                except:
-                    sensor_name='ccd' 
-                    sensor=dataid[sensor_name]
-                file_to_save.write("--%s visit=%i raft=%s %s=%s\n" % \
-                                (args.idopt, dataid['visit'], dataid['raft'], sensor_name, sensor))
+                sensor_name='ccd' 
+                sensor=dataid[sensor_name]
+            file_to_save.write("--%s visit=%i raft=%s %s=%s\n" % \
+                               (args.idopt, dataid['visit'], dataid['raft'], sensor_name, sensor))
         file_to_save.close()
         print(" - %s: %i dataids -> %s" %(filt, len(fdataids[filt]), visit_file))
 
