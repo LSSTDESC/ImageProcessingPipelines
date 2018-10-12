55621065443b4d934c8d42772ca7569a91062de2
Johann Cohen-Tanugi <johann.cohen-tanugi@umontpellier.fr>
2018-10-12 09:15:42 +0200

    turn to obs_lsst when setting up

--- workflows/srs/pipe_setups/DMsetup.sh
+++ workflows/srs/pipe_setups/DMsetup.sh
@@ -22,7 +22,7 @@ then
 else
     eups undeclare obs_lsstCam localver
     eups declare -r $ROOT_SOFTS/obs_lsstCam obs_lsstCam localver
-    eups declare -r ${ROOT_SOFTS}_bis/pipe_tasks pipe_tasks localver
-    setup obs_lsstCam localver
+    eups declare -r ${ROOT_SOFTS}/pipe_tasks pipe_tasks localver
+    setup obs_lsst localver
     setup pipe_tasks localver
 fi
