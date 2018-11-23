cf718d820b01706aeab4d839a566ab4bf55e0faa
Johann Cohen-Tanugi <johann.cohen-tanugi@umontpellier.fr>
2018-11-23 13:58:39 +0100

    change setup that has to be used for this branch

--- workflows/srs/pipe_setups/DMsetup.sh
+++ workflows/srs/pipe_setups/DMsetup.sh
@@ -20,9 +20,9 @@ if [[ $SITE == "NERSC" ]]
 then
     setup obs_lsstSim w.2018.04-35-g11f44ee -t w_2018_09
 else
-    eups undeclare obs_lsstCam localver
-    eups declare -r $ROOT_SOFTS/obs_lsstCam obs_lsstCam localver
-    eups declare -r ${ROOT_SOFTS}/pipe_tasks pipe_tasks localver
-    setup obs_lsst localver
-    setup pipe_tasks localver
+    eups undeclare obs_lsst dc2-run1.2-v2
+    eups declare -r $ROOT_SOFTS/obs_lsstCam obs_lsst dc2-run1.2-v2
+    eups declare -r ${ROOT_SOFTS}/pipe_tasks pipe_tasks u-rearmstr-desc-ccd-fix_w39
+    setup obs_lsst  dc2-run1.2-v2
+    setup pipe_tasks u-rearmstr-desc-ccd-fix_w39
 fi
