b01f3a60b8d8c39802fd3829262a000f3faf65a3
Johann Cohen-Tanugi <johann.cohen-tanugi@umontpellier.fr>
2018-11-23 13:50:27 +0100

    undo erroneous new setup that does not belong to this branch

--- workflows/srs/pipe_setups/DMsetup.sh
+++ workflows/srs/pipe_setups/DMsetup.sh
@@ -21,8 +21,8 @@ then
     setup obs_lsstSim w.2018.04-35-g11f44ee -t w_2018_09
 else
     eups undeclare obs_lsstCam localver
-    eups declare -r $ROOT_SOFTS/obs_lsstCam obs_lsstCam localver
-    eups declare -r ${ROOT_SOFTS}/pipe_tasks pipe_tasks localver
-    setup obs_lsst localver
+    eups declare --force -r ${ROOT_SOFTS}/obs_lsstCam obs_lsstCam localver
+    eups declare --force -r ${ROOT_SOFTS}/pipe_tasks pipe_tasks localver
+    setup obs_lsstCam localver
     setup pipe_tasks localver
 fi
