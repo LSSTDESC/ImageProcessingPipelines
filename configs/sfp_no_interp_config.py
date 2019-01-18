73564ab840fcea9f1b3f4b4f5c8f48f79aa2179e
Jim Chiang <jchiang@slac.stanford.edu>
2019-01-18 15:25:49 -0800

    config to disable interpolation of CRs and saturated pixels for single frame processing inputs to makeFpSummary

---
+++ configs/sfp_no_interp_config.py
@@ -0,0 +1,4 @@
+# Configuration overrides to disable interpolation for cosmic rays and
+# saturated pixels.
+config.charImage.repair.cosmicray.keepCRs=True
+config.isr.doSaturationInterpolation=False
