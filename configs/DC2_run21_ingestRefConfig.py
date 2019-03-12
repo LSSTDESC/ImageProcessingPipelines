5fc1a9430fdad62211532463a4c934913d84b557
Dominique Boutigny <boutigny@in2p3.fr>
2019-03-12 19:54:05 +0100

    change ingestReferenceCatalog.py config file in order to get column names compatible with obs_lsst

--- configs/DC2_run21_ingestRefConfig.py
+++ configs/DC2_run21_ingestRefConfig.py
@@ -12,9 +12,11 @@ config.dataset_config.indexer.name='HTM'
 config.file_reader.header_lines=1
 
 # An ordered list of column names to use in ingesting the catalog. With an empty list, column names will be discovered from the first line after the skipped header lines.
-config.file_reader.colnames=['id', 'ra', 'dec', 'sigma_ra', 'sigma_dec', 'ra_smeared', 'dec_smeared', 'u', 'sigma_u', 'g', 'sigma_g',
-                             'r', 'sigma_r', 'i', 'sigma_i', 'z', 'sigma_z', 'y', 'sigma_y', 'u_smeared', 'g_smeared', 'r_smeared',
-                             'i_smeared', 'z_smeared', 'y_smeared', 'isresolved', 'isagn', 'properMotionRa', 'properMotionDec', 'parallax', 'radialVelocity']
+config.file_reader.colnames=['id', 'ra', 'dec', 'sigma_ra', 'sigma_dec', 'ra_smeared', 'dec_smeared', 'lsst_u', 'sigma_lsst_u',
+                             'lsst_g', 'sigma_lsst_g', 'lsst_r', 'sigma_lsst_r', 'lsst_i', 'sigma_lsst_i', 'lsst_z', 'sigma_lsst_z',
+                             'lsst_y', 'sigma_lsst_y', 'lsst_u_smeared', 'lsst_g_smeared', 'lsst_r_smeared',
+                             'lsst_i_smeared', 'lsst_z_smeared', 'lsst_y_smeared', 'isresolved', 'isagn', 'properMotionRa',
+                             'properMotionDec', 'parallax', 'radialVelocity']
 
 # Delimiter to use when reading text reference files.  Comma is default.
 config.file_reader.delimiter=','
@@ -26,10 +28,11 @@ config.ra_name='ra_smeared'
 config.dec_name='dec_smeared'
 
 # The values in the reference catalog are assumed to be in AB magnitudes. List of column names to use for photometric information.  At least one entry is required.
-config.mag_column_list=['u_smeared', 'g_smeared', 'r_smeared', 'i_smeared', 'z_smeared', 'y_smeared']
+config.mag_column_list=['lsst_u_smeared', 'lsst_g_smeared', 'lsst_r_smeared', 'lsst_i_smeared', 'lsst_z_smeared', 'lsst_y_smeared']
 
 # A map of magnitude column name (key) to magnitude error column (value).
-config.mag_err_column_map={'u_smeared': 'sigma_u', 'g_smeared': 'sigma_g', 'r_smeared': 'sigma_r', 'i_smeared': 'sigma_i', 'z_smeared': 'sigma_z', 'y_smeared': 'sigma_y'}
+config.mag_err_column_map={'lsst_u_smeared': 'sigma_lsst_u', 'lsst_g_smeared': 'sigma_lsst_g', 'lsst_r_smeared': 'sigma_lsst_r',
+                           'lsst_i_smeared': 'sigma_lsst_i', 'lsst_z_smeared': 'sigma_lsst_z', 'lsst_y_smeared': 'sigma_lsst_y'}
 
 # Name of column stating if satisfactory for photometric calibration (optional).
 config.is_photometric_name=None
@@ -44,4 +47,5 @@ config.is_variable_name='isagn'
 config.id_name='id'
 
 # Extra columns to add to the reference catalog.
-config.extra_col_names=['ra', 'dec', 'sigma_ra', 'sigma_dec', 'u', 'g', 'r', 'i', 'z', 'y', 'properMotionRa', 'properMotionDec', 'parallax', 'radialVelocity']
+config.extra_col_names=['ra', 'dec', 'sigma_ra', 'sigma_dec', 'lsst_u', 'lsst_g', 'lsst_r', 'lsst_i', 'lsst_z', 'lsst_y',
+                        'properMotionRa', 'properMotionDec', 'parallax', 'radialVelocity']
