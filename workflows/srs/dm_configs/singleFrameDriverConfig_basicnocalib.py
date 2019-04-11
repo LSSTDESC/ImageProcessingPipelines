ad960015aadd8fc1c837c6f4eb3668702b0352a8
Heather Kelly <heather@slac.stanford.edu>
2019-04-11 08:10:32 -0700

    removing old DM config scripts

--- workflows/srs/dm_configs/singleFrameDriverConfig_basicnocalib.py
+++
@@ -1,9 +0,0 @@
-
-config.processCcd.isr.doDark=False
-config.processCcd.isr.doBias=False
-config.processCcd.isr.doFlat=False
-config.processCcd.calibrate.doPhotoCal=True
-config.processCcd.calibrate.doAstrometry=True
-config.processCcd.isr.doLinearize = False
-config.processCcd.isr.doDefect=False
-
