1ba7ed4c452a88e534ddcaae96e6a88344141f47
Johann Cohen-Tanugi <johann.cohen-tanugi@umontpellier.fr>
2019-05-07 00:46:50 +0200

    update the IPP setup

--- workflows/srs/pipe_setups/DMsetup.sh
+++ workflows/srs/pipe_setups/DMsetup.sh
@@ -19,10 +19,24 @@ if [[ $SITE == "NERSC" ]]
 then
     setup -r ${ROOT_SOFTS}/obs_lsst -j
 else
-    eups undeclare obs_lsst dc2-run1.2-v3
-    eups declare -r $ROOT_SOFTS/obs_lsst obs_lsst dc2-run1.2-v3
-    eups declare -r ${ROOT_SOFTS}/pipe_tasks pipe_tasks u-rearmstr-desc-ccd-fix_w39
-    setup obs_lsst  dc2-run1.2-v3
-    setup pipe_tasks u-rearmstr-desc-ccd-fix_w39
+    # eups undeclare obs_lsst dc2-run1.2-v3
+    # eups declare -r $ROOT_SOFTS/obs_lsst obs_lsst dc2-run1.2-v3
+    # eups declare -r ${ROOT_SOFTS}/pipe_tasks pipe_tasks u-rearmstr-desc-ccd-fix_w39
+    # setup obs_lsst  dc2-run1.2-v3
+    # setup pipe_tasks u-rearmstr-desc-ccd-fix_w39
+
+    eups declare -r $ROOT_SOFTS/obs_lsst obs_lsst dc2-run2.1
+    setup obs_lsst  dc2-run2.1
+    eups declare -r $ROOT_SOFTS/meas_extensions_ngmix meas_extensions_ngmix dc2-run2.1
+    setup meas_extensions_ngmix  dc2-run2.1
+
+    # setup obs_lsst
+    # eups declare -r ${ROOT_SOFTS}/meas_extensions_ngmix meas_extensions_ngmix local_w13
+    # setup meas_extensions_ngmix local_w13
+
+    export PYTHONPATH=${ROOT_SOFTS}/ngmix/build/lib:$PYTHONPATH
+    eups list obs_lsst
+    eups list meas_extensions_ngmix
+    python -c "import ngmix"
 fi
 eups list obs_lsst
