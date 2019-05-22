093e5eb95ff05efa8f00cc1d7cb9d2fbb8cff174
Johann Cohen-Tanugi <johann.cohen-tanugi@umontpellier.fr>
2019-05-22 16:58:25 +0200

    update setups and add a checkpoint function

--- workflows/srs/pipe_setups/DMsetup.sh
+++ workflows/srs/pipe_setups/DMsetup.sh
@@ -10,6 +10,21 @@
 export HOME=`pwd`
 #fi
 
+#utility functions
+verify_checkpoint () {
+    var=`grep $1 ${CHECKPOINTS}`
+    #check that process is not commented
+    if [ "${var%${var#?}}"x != '#x' ]
+    then
+        local status=`echo ${var: -3:1}`
+    else
+        local status=""
+    fi
+    echo $status
+} 
+
+
+
 # Setup for the stack
 source ${DM_SETUP}
 setup lsst_distrib
@@ -25,18 +40,29 @@ else
     # setup obs_lsst  dc2-run1.2-v3
     # setup pipe_tasks u-rearmstr-desc-ccd-fix_w39
 
+    echo "setting obs_lsst"
+    eups undeclare obs_lsst dc2-run2.1
     eups declare -r $ROOT_SOFTS/obs_lsst obs_lsst dc2-run2.1
     setup obs_lsst  dc2-run2.1
+    eups list obs_lsst
+
+    echo "setting meas_extensions_ngmix"
+    eups undeclare meas_extensions_ngmix  dc2-run2.1
     eups declare -r $ROOT_SOFTS/meas_extensions_ngmix meas_extensions_ngmix dc2-run2.1
     setup meas_extensions_ngmix  dc2-run2.1
-
-    # setup obs_lsst
-    # eups declare -r ${ROOT_SOFTS}/meas_extensions_ngmix meas_extensions_ngmix local_w13
-    # setup meas_extensions_ngmix local_w13
-
     export PYTHONPATH=${ROOT_SOFTS}/ngmix/build/lib:$PYTHONPATH
-    eups list obs_lsst
     eups list meas_extensions_ngmix
+
+    echo "checking ngmix availability..."
     python -c "import ngmix"
+    echo "done"
+
+    echo "setting pipe_analysis"
+    eups undeclare pipe_analysis dc2-run2.1
+    eups declare -r $ROOT_SOFTS/pipe_analysis pipe_analysis dc2-run2.1
+    setup display_matplotlib
+    setup meas_extensions_astrometryNet
+    setup pipe_analysis  dc2-run2.1
+    eups list pipe_analysis
 fi
-eups list obs_lsst
+
