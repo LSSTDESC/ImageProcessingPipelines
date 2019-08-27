25e136a62a511a831467e888af9a3f35095fb619
Johann Cohen-Tanugi <johann.cohen-tanugi@umontpellier.fr>
2019-08-27 15:12:08 +0200

    fix #123

--- workflows/srs/pipe_scripts/merge_registries.sh
+++ workflows/srs/pipe_scripts/merge_registries.sh
@@ -1,4 +1,4 @@
-#!/bin/env bash
+#!/bin/bash
 
 final_db=$3
 db_list=$2
@@ -14,10 +14,11 @@ for db in $db_list; do
 	    count=`sqlite3 ${final_db} "select count(*) from overlaps;";`
 	    offset=`sqlite3 ${db} "select count(*) from overlaps;";`
 	    echo "adding $offset to $count rows"
-	    if [ "$1" == "TRACT2VISIT"]; then
+	    if [ "$1" == "TRACT2VISIT" ]; then
 		sqlite3 $final_db "attach '$db' as toMerge;insert into overlaps select id+$count,tract,patch,visit,detector,filter,layer from toMerge.overlaps;"
-	    elif [ "$1" == "RAW2VISIT"]; then 
+	    elif [ "$1" == "RAW2VISIT" ]; then 
 		sqlite3 $final_db "attach '$db' as toMerge;insert into raw select id+$count,run,visit,filter,date,dateObs,expTime,raftName,detectorName,detector,snap,object,imageType,testType,lsstSerial,wavelength from toMerge.overlaps; insert into raw_visit select visit,filter,dateObs,expTime from toMerge.overlaps;"
+	    fi
 	fi
     else
 	echo "cp $db $final_db"
