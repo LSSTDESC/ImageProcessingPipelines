bb0b1467be190f5e95f087539e67199104fe5534
Ben Clifford <benc@hawaga.org.uk>
2019-09-04 05:54:44 -0700

    use parsl from git checkout, rather than awkwardly installed one

--- workflows/parsl-benc/launch_process_worker_pool.py
+++ workflows/parsl-benc/launch_process_worker_pool.py
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 
-shifter --image=lsstdesc/stack-sims:w_2019_19-sims_w_2019_19 bash -c "source /opt/lsst/software/stack/loadLSST.bash ; setup lsst_distrib ; setup obs_lsst ; /global/homes/b/bxc/.local/cori/3.7-anaconda-2019.07/bin/process_worker_pool.py $*"
+shifter --image=lsstdesc/stack-sims:w_2019_19-sims_w_2019_19 bash -c "source /opt/lsst/software/stack/loadLSST.bash ; setup lsst_distrib ; setup obs_lsst ; export PYTHONPATH=/global/homes/b/bxc/dm/parsl:$PYTHONPATH; /global/homes/b/bxc/dm/parsl/parsl/executors/high_throughput/process_worker_pool.py $*"
 
