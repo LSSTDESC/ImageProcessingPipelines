3ac043e1ee2b4ac65631ef92a71ebf0d4842bc45
Ben Clifford <benc@hawaga.org.uk>
2019-11-09 11:22:36 -0800

    remove unneeded paths

--- workflows/parsl-benc/container-inner.sh
+++ workflows/parsl-benc/container-inner.sh
@@ -6,8 +6,6 @@ shift
 source /opt/lsst/software/stack/loadLSST.bash
 setup lsst_distrib
 setup obs_lsst
-export PYTHONPATH=/global/homes/b/bxc/dm/parsl:$PYTHONPATH
-export PATH=/global/homes/b/bxc/dm/parsl/parsl/executors/high_throughput/:$PATH
 
 export export OMP_NUM_THREADS=1
 
