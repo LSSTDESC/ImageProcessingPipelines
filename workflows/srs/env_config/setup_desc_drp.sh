ae58be060aba85a176bdfe597598145400e9754b
Johann Cohen-Tanugi <johann.cohen-tanugi@umontpellier.fr>
2019-12-07 09:18:24 +0100

    revert changes on this file, which is dealt with in https://github.com/LSSTDESC/ImageProcessingPipelines/pull/132#

--- workflows/srs/env_config/setup_desc_drp.sh
+++ workflows/srs/env_config/setup_desc_drp.sh
@@ -7,7 +7,7 @@ then
 fi
 
 export STACKCVMFS=/cvmfs/sw.lsst.eu/linux-x86_64/lsst_distrib
-export DESC_STACK_VER=$2
+export DESC_STACK_VER=v18.1.0-dev
 
 echo "${DESC_STACK_VER}" > $1/stack_version
 
@@ -26,37 +26,23 @@ fi
 
 
 # Version List
-#export DESC_pyarrow_VER=0.13.0
+export DESC_pyarrow_VER=0.13.0
 export DESC_GCR_VER=0.8.8
 export DESC_GCRCatalogs_VER=v0.14.3
 export DESC_ngmix_VER=1.3.4
 export DESC_ngmix_VER_STR=v$DESC_ngmix_VER
-export DESC_meas_extensions_ngmix_VER=0.9.5
-export DESC_obs_lsst_VER=18.1.0-run2.2-v1
+export DESC_meas_extensions_ngmix_VER=0.9.4
 
 source $STACKCVMFS/$DESC_STACK_VER/loadLSST.bash
 
 # pip install what we can, using a constraints file and local root directory
-#pip install -c ./dm-constraints-py3-4.5.12.txt --prefix $1 pyarrow==$DESC_pyarrow_VER
+pip install -c ./dm-constraints-py3-4.5.12.txt --prefix $1 pyarrow==$DESC_pyarrow_VER
 pip install -c ./dm-constraints-py3-4.5.12.txt --prefix $1 GCR==$DESC_GCR_VER
 pip install -c ./dm-constraints-py3-4.5.12.txt --prefix $1 https://github.com/LSSTDESC/gcr-catalogs/archive/$DESC_GCRCatalogs_VER.tar.gz
 
+# Install ngmix, requires numba which is already included in DM env
 curdir=$PWD
 cd $1
-
-#install obs_lsst
-git clone git@github.com:lsst/obs_lsst.git
-cd obs_lsst
-git checkout $DESC_obs_lsst_VER
-setup -r . -j 
-scons
-eups declare -r . obs_lsst dc2-run2.2
-cd ..
-
-#install DC2-production
-git clone git@github.com:LSSTDESC/DC2-production.git
-
-# Install ngmix, requires numba which is already included in DM env
 curl -LO https://github.com/esheldon/ngmix/archive/$DESC_ngmix_VER_STR.tar.gz
 tar xzf $DESC_ngmix_VER_STR.tar.gz
 cd ngmix-$DESC_ngmix_VER
