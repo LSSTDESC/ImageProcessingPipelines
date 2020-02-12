fed1bf2fb7c56727084b17d0934472153be86379
Ben Clifford <benc@hawaga.org.uk>
2020-02-12 09:04:40 -0800

    store logs in rundir, but make checkpointing ignore the log path

--- workflows/parsl-benc/workflow.py
+++ workflows/parsl-benc/workflow.py
@@ -26,6 +26,9 @@ configuration = configuration.load_configuration()
 
 parsl.load(configuration.parsl_config)
 
+logdir = parsl.dfk().run_dir + "/dm-logs/"
+logger.info("Log directory is " + logdir)
+
 ingest_future = ingest.perform_ingest(configuration)
 
 logger.info("waiting for ingest(s) to complete")
@@ -44,14 +47,14 @@ logger.info("ingest(s) completed")
 
 # ingest list is passed in but not used explicity because it represents that some stuff
 # has gone into the DB potentially during ingest - for checkpointing
-@bash_app(executors=["worker-nodes"], cache=True)
+@bash_app(executors=["worker-nodes"], cache=True, ignore_for_checkpointing=["stdout", "stderr"])
 def make_sky_map(wrap, in_dir, rerun, stdout=None, stderr=None):
     return wrap("makeSkyMap.py {} --rerun {}".format(in_dir, rerun))
 
 
 logger.info("launching makeSkyMap")
 rerun = configuration.rerun
-skymap_future = make_sky_map(configuration.wrap, configuration.in_dir, rerun, stdout="logs/make_sky_map.stdout", stderr="logs/make_sky_map.stderr")
+skymap_future = make_sky_map(configuration.wrap, configuration.in_dir, rerun, stdout=logdir+"make_sky_map.stdout", stderr=logdir+"make_sky_map.stderr")
 skymap_future.result()
 logger.info("makeSkyMap completed")
 
@@ -59,7 +62,7 @@ logger.info("makeSkyMap completed")
 logger.info("Making visit file from raw_visit table")
 
 
-@bash_app(executors=["worker-nodes"], cache=True)
+@bash_app(executors=["worker-nodes"], cache=True,  ignore_for_checkpointing=["stdout", "stderr"])
 def make_visit_file(wrap, in_dir, stdout=None, stderr=None):
     return wrap('sqlite3 {}/registry.sqlite3 "select DISTINCT visit from raw_visit;" > all_visits_from_register.list'.format(in_dir))
 
@@ -67,8 +70,8 @@ def make_visit_file(wrap, in_dir, stdout=None, stderr=None):
 visit_file_future = make_visit_file(
     configuration.wrap,
     configuration.in_dir,
-    stdout="logs/make_visit_file.stdout",
-    stderr="logs/make_visit_file.stderr")
+    stdout=logdir+"make_visit_file.stdout",
+    stderr=logdir+"make_visit_file.stderr")
 
 visit_file_future.result()
 
@@ -77,7 +80,7 @@ logger.info("Finished making visit file")
 logger.info("submitting task_calexps")
 
 
-@bash_app(executors=["worker-nodes"], cache=True)
+@bash_app(executors=["worker-nodes"], cache=True,  ignore_for_checkpointing=["stdout", "stderr"])
 def single_frame_driver(wrap, in_dir, rerun, visit_id, raft_name, stdout=None, stderr=None):
     # params for stream are WORKDIR=workdir, VISIT=visit_id
     # this is going to be something like found in workflows/srs/pipe_setups/run_calexp
@@ -89,7 +92,7 @@ def single_frame_driver(wrap, in_dir, rerun, visit_id, raft_name, stdout=None, s
     return wrap("singleFrameDriver.py --batch-type none {in_dir} --rerun {rerun} --id visit={visit} raftName={raft_name} --clobber-versions --cores 1 --timeout 999999999 --loglevel CameraMapper=warn".format(in_dir=in_dir, rerun=rerun, visit=visit_id, raft_name=raft_name))
 
 
-@bash_app(executors=["worker-nodes"], cache=True)
+@bash_app(executors=["worker-nodes"], cache=True,  ignore_for_checkpointing=["stdout", "stderr"])
 def raft_list_for_visit(wrap, in_dir, visit_id, out_filename, stderr=None, stdout=None):
     return wrap("sqlite3 {in_dir}/registry.sqlite3 'select distinct raftName from raw where visit={visit_id}' > {out_filename}".format(in_dir=in_dir, visit_id=visit_id, out_filename=out_filename))
 
@@ -98,14 +101,14 @@ def raft_list_for_visit(wrap, in_dir, visit_id, out_filename, stderr=None, stdou
 # specified visit - I'm not sure quite the right way to do it, and I think its only
 # useful in during workflow development when the original ingest list might change?
 # would need eg "files in each visit" list to generate a per-visit input "version" id/hash
-@bash_app(executors=["worker-nodes"], cache=True)
+@bash_app(executors=["worker-nodes"], cache=True,  ignore_for_checkpointing=["stdout", "stderr"])
 def check_ccd_astrometry(wrap, root_softs, in_dir, rerun, visit, inputs=[], stderr=None, stdout=None):
     # inputs=[] ignored but used for dependency handling
     return wrap("{root_softs}/ImageProcessingPipelines/python/util/checkCcdAstrometry.py {in_dir}/rerun/{rerun} --id visit={visit} --loglevel CameraMapper=warn".format(visit=visit, rerun=rerun, in_dir=in_dir, root_softs=root_softs))
 
 # the parsl checkpointing for this won't detect if we ingested more stuff to do with the
 # specified visit - see comments for check_ccd_astrometry
-@bash_app(executors=["worker-nodes"], cache=True)
+@bash_app(executors=["worker-nodes"], cache=True,  ignore_for_checkpointing=["stdout", "stderr"])
 def tract2visit_mapper(wrap, root_softs, in_dir, rerun, visit, inputs=[], stderr=None, stdout=None):
     # TODO: this seems to be how $REGISTRIES is figured out (via $WORKDIR) perhaps?
     # I'm unsure though
@@ -114,7 +117,7 @@ def tract2visit_mapper(wrap, root_softs, in_dir, rerun, visit, inputs=[], stderr
     return wrap("mkdir -p {registries} && {root_softs}/ImageProcessingPipelines/python/util/tract2visit_mapper.py --indir={in_dir}/rerun/{rerun} --db={registries}/tracts_mapping_{visit}.sqlite3 --visits={visit}".format(in_dir=in_dir, rerun=rerun, visit=visit, registries=registries, root_softs=root_softs))
 
 
-@bash_app(executors=["worker-nodes"], cache=True)
+@bash_app(executors=["worker-nodes"], cache=True,  ignore_for_checkpointing=["stdout", "stderr"])
 def sky_correction(wrap, in_dir, rerun, visit, inputs=[], stdout=None, stderr=None):
     return wrap("skyCorrection.py {in_dir}  --rerun {rerun} --id visit={visit} --batch-type none --cores 1 --timeout 999999999 --no-versions --loglevel CameraMapper=warn".format(in_dir=in_dir, rerun=rerun, visit=visit))
 
@@ -137,8 +140,8 @@ for (n, visit_id_unstripped) in zip(range(0, len(visit_lines)), visit_lines):
         configuration.in_dir,
         visit_id,
         raft_list_fn,
-        stdout="logs/"+raft_list_fn+".stdout",
-        stderr="logs/"+raft_list_fn+".stderr")
+        stdout=logdir+raft_list_fn+".stdout",
+        stderr=logdir+raft_list_fn+".stderr")
     
     raft_list_future.result()
     # this wait here means that we don't get parallelisation so much
@@ -166,8 +169,8 @@ for (n, visit_id_unstripped) in zip(range(0, len(visit_lines)), visit_lines):
                 rerun,
                 visit_id,
                 raft_name,
-                stdout="logs/"+sfd_output_basename+".stdout",
-                stderr="logs/"+sfd_output_basename+".stderr")
+                stdout=logdir+sfd_output_basename+".stdout",
+                stderr=logdir+sfd_output_basename+".stderr")
         )
 
     # now need to join based on all of this_visit_single_frame_futs... but not in sequential code
@@ -189,8 +192,8 @@ for (n, visit_id_unstripped) in zip(range(0, len(visit_lines)), visit_lines):
         rerun,
         visit_id,
         inputs=this_visit_single_frame_futs,
-        stdout="logs/"+check_ccd_stdbase+".stdout",
-        stderr="logs/"+check_ccd_stdbase+".stderr")
+        stdout=logdir+check_ccd_stdbase+".stdout",
+        stderr=logdir+check_ccd_stdbase+".stderr")
 
 
     tract2visit_mapper_stdbase = "track2visit_mapper.{}".format(visit_id)
@@ -201,8 +204,8 @@ for (n, visit_id_unstripped) in zip(range(0, len(visit_lines)), visit_lines):
         rerun,
         visit_id,
         inputs=[fut1],
-        stdout="logs/"+tract2visit_mapper_stdbase+".stdout",
-        stderr="logs/"+tract2visit_mapper_stdbase+".stderr")
+        stdout=logdir+tract2visit_mapper_stdbase+".stdout",
+        stderr=logdir+tract2visit_mapper_stdbase+".stderr")
 
     # this is invoked in run_calexp with $OUT_DIR at the first parameter, but that's not something
     # i've used so far -- so I'm using IN_DIR as used in previous steps
@@ -214,8 +217,8 @@ for (n, visit_id_unstripped) in zip(range(0, len(visit_lines)), visit_lines):
         rerun,
         visit_id,
         inputs=[fut2],
-        stdout="logs/"+sky_correction_stdbase+".stdout",
-        stderr="logs/"+sky_correction_stdbase+".stderr")
+        stdout=logdir+sky_correction_stdbase+".stdout",
+        stderr=logdir+sky_correction_stdbase+".stderr")
     
     calexp_futs.append(fut3)
 
