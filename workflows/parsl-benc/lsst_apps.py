1fa5fb95d4d8a3a633177968b497fe03ca2350dc
Ben Clifford <benc@hawaga.org.uk>
2020-07-21 04:16:44 -0700

    fix flake8 for lsst_apps.py

--- workflows/parsl-benc/lsst_apps.py
+++ workflows/parsl-benc/lsst_apps.py
@@ -1,11 +1,9 @@
 from parsl import bash_app
 
-# This defines a decorator lsst_app which captures the options that
-# most of the core application code will need
 lsst_app1 = bash_app(executors=["batch-1"],
-                    cache=True,
-                    ignore_for_cache=["stdout", "stderr", "wrap", "parsl_resource_specification"])
-lsst_app2 = bash_app(executors=["batch-2"],
-                    cache=True,
-                    ignore_for_cache=["stdout", "stderr", "wrap", "parsl_resource_specification"])
+                     cache=True,
+                     ignore_for_cache=["stdout", "stderr", "wrap", "parsl_resource_specification"])
 
+lsst_app2 = bash_app(executors=["batch-2"],
+                     cache=True,
+                     ignore_for_cache=["stdout", "stderr", "wrap", "parsl_resource_specification"])
